9a1a6bc41b8e4c89b2604a772318807781fdc063
kpuwar <kpuwar@student.42heilbronn.de>
2023-12-28 08:55:36 +0100

    Makefile&main.cpp done, MutantStack.hpp in progres

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 07:53:14 by kpuwar            #+#    #+#             */
+/*   Updated: 2023/12/28 08:53:34 by kpuwar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class MutantStack
+{
+	private:
+		
+	public:
+		MutantStack();
+		~MutantStack();
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/28 07:52:15 by kpuwar            #+#    #+#             */
+/*   Updated: 2023/12/28 08:52:35 by kpuwar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	std::stack<int> s(mstack);
+
+	return 0;
+}

e28de2bfb9ff8f9f5dde9f65500a6805727b434d
kpuwar <kpuwar@student.42heilbronn.de>
2023-12-28 21:20:59 +0100

    ex02 done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 07:53:14 by kpuwar            #+#    #+#             */
-/*   Updated: 2023/12/28 08:53:34 by kpuwar           ###   ########.fr       */
+/*   Updated: 2023/12/28 21:16:30 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,75 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <stack>
 
 using std::cout;
 using std::endl;
+using std::stack;
 
-class MutantStack
+template <typename T>
+class MutantStack : public stack<T>
 {
-	private:
-		
 	public:
-		MutantStack();
-		~MutantStack();
+		typedef typename stack<T>::container_type::iterator iterator;
+
+		typedef typename stack<T>::container_type::const_iterator const_iterator;
+
+		MutantStack() {
+			
+		}
+
+		MutantStack(const MutantStack &src) {
+			*this = src;
+		}
+
+		MutantStack& operator=(const MutantStack &rhs) {
+			stack<T>::operator=(rhs);
+			return *this;
+		}
+
+		~MutantStack() {
+			
+		}
+
+		iterator begin()
+		{
+			return this->c.begin();
+		}
+
+		iterator end()
+		{
+			return this->c.end();
+		}
+
+		const_iterator cbegin() const
+		{
+			return this->c.begin();
+		}
+
+		const_iterator cend() const
+		{
+			return this->c.end();
+		}
 };
+
+template <typename T>
+std::ostream& operator<<(std::ostream& o, MutantStack<T>& rhs) {
+	if (rhs.size() < 1) {
+		o << "---" << endl;
+		o << "[] <- top" << endl;
+		o << "---";
+	} else {
+		o << "---" << endl;
+		for (typename MutantStack<T>::iterator i = rhs.begin(); i != rhs.end(); i++) {
+			if (i + 1 == rhs.end()) {
+				o << "[" << *i << "] <- top" << endl;
+				o << "---";
+			} else
+				o << "[" << *i << "]" << endl;
+		}
+	}
+	return o;
+}
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 07:52:15 by kpuwar            #+#    #+#             */
-/*   Updated: 2023/12/28 08:52:35 by kpuwar           ###   ########.fr       */
+/*   Updated: 2023/12/28 21:19:16 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main()
+int main(void)
 {
 	MutantStack<int> mstack;
 
@@ -35,14 +35,14 @@ int main()
 
 	++it;
 	--it;
-
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
-
 	std::stack<int> s(mstack);
+
+	cout << mstack << endl;
 
 	return 0;
 }
